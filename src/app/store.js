0885b225a52b62f312002b45240d76b5a4b08c7f
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-17 20:30:38 +0200

    counterReducer fix

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
 //Create store
 export const store = configureStore({
-  reducer: {},
+  reducer: counterReducer,
 });
