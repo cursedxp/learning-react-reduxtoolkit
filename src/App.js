f8131686ed61b5ababc7b6c718f802505bc0ce75
Anil Ozsoy <anilozsoy@gmail.com>
2023-04-18 15:01:46 +0200

    userlist added to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Counter from "./features/counter/counter.jsx";
-
+import UserList from "./features/userlist/userList";
 function App() {
   return (
     <div className="App">
       <Counter />
+      <UserList />
     </div>
   );
 }
